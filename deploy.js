7aae9a6b9f50d4ee1747207ccdb8ce904196dedf
antargt <aragonanthonyj@gmail.com>
2021-02-22 00:33:20 -0700

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: 'ec2-54-196-168-193.compute-1.amazonaws.com',
+      host: '0.0.0.0',
       username: 'ubuntu',
       privateKey: 'C:\Users\arago\Desktop\Programming\todo1-key.pem'
     })
